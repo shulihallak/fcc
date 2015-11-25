9e2d7d4f5d04fd7d015265b72c5ba3b0a7cad0c9
shulihallak <shuli@invisiblenetworks.co>
2015-11-25 17:07:47 -0500

    some d3 stuff

--- js/map.js
+++ js/map.js
@@ -1,7 +1,65 @@
+// This example creates circles on the map, representing populations in North
+// America.
+
+// First, create an object containing LatLng and population for each city.
+// var citymap = {
+//   chicago: {
+//     center: {lat: 41.878, lng: -87.629},
+//     population: 2714856
+//   },
+//   newyork: {
+//     center: {lat: 40.714, lng: -74.005},
+//     population: 8405837
+//   },
+//   losangeles: {
+//     center: {lat: 34.052, lng: -118.243},
+//     population: 3857799
+//   },
+//   vancouver: {
+//     center: {lat: 49.25, lng: -123.1},
+//     population: 603502
+//   }
+// };
+//
+// function initMap() {
+//   // Create the map.
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 4,
+//     center: {lat: 37.090, lng: -95.712},
+//     mapTypeId: google.maps.MapTypeId.TERRAIN
+//   });
+//
+//   // Construct the circle for each value in citymap.
+//   // Note: We scale the area of the circle based on the population.
+//   for (var city in citymap) {
+//     // Add the circle for this city to the map.
+//     var cityCircle = new google.maps.Circle({
+//       strokeColor: '#FF0000',
+//       strokeOpacity: 0.8,
+//       strokeWeight: 2,
+//       fillColor: '#FF0000',
+//       fillOpacity: 0.35,
+//       map: map,
+//       center: citymap[city].center,
+//       radius: Math.sqrt(citymap[city].population) * 100
+//     });
+//   }
+// }
+
+
+      var citymap = {
+        city: {
+          center: {lat: 40.7127837, lng: -74.00594130000002},
+          wirelessDown: 32478.8
+        }
+      };
+
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 5,
-    center: {lat: 40.713, lng: -74.006},
+    zoom: 4,
+    center: {lat: 37.090, lng: -95.712},
+    mapTypeId: google.maps.MapTypeId.TERRAIN
     // zoomControl: true,
     // zoomControlOptions: {
     //   style: google.maps.ZoomControlStyle.LARGE,
@@ -17,17 +75,21 @@ function initMap() {
 }
 
 function geocodeAddress(geocoder, resultsMap) {
+  var that = this;
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === google.maps.GeocoderStatus.OK) {
       resultsMap.setCenter(results[0].geometry.location);
-      console.log(results[0]['geometry']['location'].lat());
-      console.log(results[0]['geometry']['location'].lng());
+      console.log(results[0].geometry.location.lat());
+      console.log(results[0].geometry.location.lng());
 
 
       var lat = (results[0]['geometry']['location'].lat()).toFixed(1);
       var lng = (results[0]['geometry']['location'].lng()).toFixed(1);
 
+      that.lat = (results[0]['geometry']['location'].lat()).toFixed(1);
+
+      that.lng = (results[0]['geometry']['location'].lng()).toFixed(1);
       console.log(lat);
       console.log(lng);
 
@@ -57,44 +119,27 @@ function geocodeAddress(geocoder, resultsMap) {
         (data.SpeedTestCounty.wirelineTests)*0.001;
 
         console.log(ctrl.wirelessDown);
-        //
-        // var heatmapData = [
-        //   ctrl.wirelessDown,
-        //   ctrl.wirelessUp,
-        //   ctrl.wirelessMaxDown,
-        //   ctrl.wirelessMaxUp,
-        //   ctrl.wirelessTests,
-        //   ctrl.AvgDown,
-        //   ctrl.AvgUp,
-        //   ctrl.maxDown,
-        //   ctrl.maxUp,
-        //   ctrl.tests
-        // ];
-        // var heatmapData = [
-        //   new google.maps.LatLng(37.782, -122.447),
-        //   new google.maps.LatLng(37.782, -122.445),
-        //   new google.maps.LatLng(37.782, -122.443),
-        //   new google.maps.LatLng(37.782, -122.441),
-        //   new google.maps.LatLng(37.782, -122.439),
-        //   new google.maps.LatLng(37.782, -122.437),
-        //   new google.maps.LatLng(37.782, -122.435),
-        //   new google.maps.LatLng(37.785, -122.447),
-        //   new google.maps.LatLng(37.785, -122.445),
-        //   new google.maps.LatLng(37.785, -122.443),
-        //   new google.maps.LatLng(37.785, -122.441),
-        //   new google.maps.LatLng(37.785, -122.439),
-        //   new google.maps.LatLng(37.785, -122.437),
-        //   new google.maps.LatLng(37.785, -122.435)
-        // ];
-        // var heatmap = new google.maps.visualization.HeatmapLayer({
-        //   data: heatMapData
-        // });
-        // heatmap.setMap(map);
+
       });
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
+
+    var marker = new google.maps.Marker({
+      map: resultsMap,
+      position: results[0].geometry.location,
+
+    });
+    for (var city in citymap) {
+      var dataCircle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+              map: map,
+              center: citymap[city].center,
+              radius: Math.sqrt(citymap[city].wirelessDown) * 100
       });
+    }
+
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
